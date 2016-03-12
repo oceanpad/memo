d55f776debab9a12be56a1c6b7945da2afd78614
haiyang zhao <haiyang.zhao.ikina@gmail.com>
2016-03-12 07:23:32 +0000

    start my blog

---
+++ my.scala
@@ -0,0 +1,37 @@
+
+
+
+
+>>>>>>>scala
+>>>>>>>java
+>>>>>>>playFramework
+>>>>>>>scalike jdbc
+>>>>>>>skinny ORM
+.................
+
+I
+DONT
+want 
+you 
+see 
+my 
+company's
+program
+,
+so
+Just
+see my old old old first
+scala sample
+
+
+
+def count(x:Int){
+	for(t <- x to 0 by -1) println(t)
+}
+
+def uni(x:String){
+	var ti =1
+	for(t <- x) {ti = t*ti;println(ti)}
+
+	println(ti)
+}
